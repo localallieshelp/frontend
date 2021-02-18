dd4f7fef05071b9e43aa3148e26345801996e07c
Michael Fuery <mfuery@gmail.com>
2021-02-17 17:06:31 -0800

    Update menuTree.js

--- src/data/menuTree.js
+++ src/data/menuTree.js
@@ -1,5 +1,5 @@
-// {<token>: [en uri, cn uri], ...}
 module.exports = {
+  // {<token>: [en uri, cn uri], ...}
   story: ["story", "story"],
   business: ["business", "business"],
   contact: ["contact", "contact"],
