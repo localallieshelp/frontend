c4e283f2600ac5a9e919dc830d8d0589023e4e40
Michael Fuery <mfuery@gmail.com>
2021-02-18 22:38:56 -0800

    SEO/schema.org adjustments.

--- src/components/SEO/SchemaOrg.js
+++ src/components/SEO/SchemaOrg.js
@@ -9,7 +9,7 @@ export default React.memo(
     defaultTitle,
     description,
     image,
-    isBlogPost,
+    isBusinessPost,
     organization,
     title,
     url,
@@ -24,27 +24,12 @@ export default React.memo(
       },
     ]
 
-    const schema = isBlogPost
+    const schema = isBusinessPost
       ? [
           ...baseSchema,
           {
             "@context": "http://schema.org",
-            "@type": "BreadcrumbList",
-            itemListElement: [
-              {
-                "@type": "ListItem",
-                position: 1,
-                item: {
-                  "@id": url,
-                  name: title,
-                  image,
-                },
-              },
-            ],
-          },
-          {
-            "@context": "http://schema.org",
-            "@type": "BlogPosting",
+            "@type": "Organization",
             url,
             name: title,
             alternateName: defaultTitle,
@@ -54,21 +39,10 @@ export default React.memo(
               url: image,
             },
             description,
-            author: {
-              "@type": "Person",
-              name: author.name,
-            },
-            publisher: {
-              "@type": "Organization",
-              url: organization.url,
-              logo: organization.logo,
-              name: organization.name,
-            },
             mainEntityOfPage: {
               "@type": "WebSite",
               "@id": siteUrl,
             },
-            datePublished,
           },
         ]
       : baseSchema
