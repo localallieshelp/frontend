9cc71dbf560be460965c8e808a0bc0d8293835c0
Michael Fuery <mfuery@gmail.com>
2021-03-04 19:35:03 -0800

    Refactor DonateForm: remove square's module/replace with custom html + styles (per sdk docs). WIP: not yet loading iframes from square script. Add Formik validation.

--- src/components/DonateForm.js
+++ src/components/DonateForm.js
@@ -1,14 +1,9 @@
 import React from "react"
-import {
-  SquarePaymentForm,
-  CreditCardNumberInput,
-  CreditCardExpirationDateInput,
-  CreditCardPostalCodeInput,
-  CreditCardCVVInput,
-  CreditCardSubmitButton,
-} from "react-square-payment-form"
-import "react-square-payment-form/lib/default.css"
+import "./DonateForm.css"
 import PropTypes from "prop-types"
+import { Formik, Form, Field } from "formik"
+import * as Yup from "yup"
+import { FaExclamationTriangle, FaRegEnvelope, FaCheck } from "react-icons/fa"
 
 export const loadSquareSdk = () => {
   return new Promise((resolve, reject) => {
@@ -27,66 +22,333 @@ export const loadSquareSdk = () => {
   })
 }
 
+const DonateFormSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  donationAmount: Yup.string().required("Required"),
+})
+
+var paymentForm = null
+
+function onGetCardNonce(event) {
+  // Don't submit the form until SqPaymentForm returns with a nonce
+  event.preventDefault()
+  // Request a nonce from the SqPaymentForm object
+  this.paymentForm.requestCardNonce()
+}
+
 export default class DonateForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       errorMessages: [],
+      verificationDetails: {
+        amount: 0,
+        currencyCode: "USD",
+        intent: "CHARGE",
+        billingContact: {
+          familyName: "BLANK",
+          givenName: "BLANK",
+          email: "BLANK",
+          country: "BLANK",
+          city: "BLANK",
+          addressLines: ["BLANK"],
+          postalCode: "BLANK",
+          phone: "BLANK",
+        },
+      },
     }
   }
 
-  cardNonceResponseReceived(errors, nonce, cardData, buyerVerificationToken) {
-    if (errors) {
-      this.setState({ errorMessages: errors.map((error) => error.message) })
-      return
-    }
-
-    this.setState({ errorMessages: [] })
-    this.props.cardNonceResponseReceived.call(
-      this,
-      nonce,
-      cardData,
-      buyerVerificationToken
+  componentDidMount() {
+    loadSquareSdk().then(
+      () => {
+        console.log("loadSquareSdk succeeded")
+        this.paymentForm = new SqPaymentForm({
+          applicationId: this.props.applicationId,
+          locationId: this.props.locationId,
+          autoBuild: true,
+          inputClass: "sq-input",
+          inputStyles: [
+            {
+              fontSize: "16px",
+              lineHeight: "24px",
+              padding: "16px",
+              placeholderColor: "#a0a0a0",
+              backgroundColor: "transparent",
+            },
+          ],
+          cardNumber: {
+            elementId: "sq-card-number",
+            placeholder: "Card Number",
+          },
+          cvv: {
+            elementId: "sq-cvv",
+            placeholder: "CVV",
+          },
+          expirationDate: {
+            elementId: "sq-expiration-date",
+            placeholder: "MM/YY",
+          },
+          postalCode: {
+            elementId: "sq-postal-code",
+            placeholder: "Postal",
+          },
+          // SqPaymentForm callback functions
+          callbacks: {
+            cardNonceResponseReceived: this.cardNonceResponseReceived,
+          },
+        })
+      },
+      (error) => {
+        console.error("loadSquareSdk failed: ", error)
+      }
     )
   }
 
-  createVerificationDetails() {
-    return this.props.createVerificationDetails.bind(this)
+  cardNonceResponseReceived(
+    errors,
+    nonce,
+    cardData,
+    buyerVerificationToken // TODO be more secure
+  ) {
+    console.log(
+      "nonce created: " +
+        nonce +
+        ", buyerVerificationToken: " +
+        buyerVerificationToken
+    )
+    console.log("cardData")
+    console.log(cardData)
+
+    fetch("/.netlify/functions/square", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        Object.assign(
+          {
+            nonce: nonce,
+            idempotency_key: uuidv4(),
+            location_id: process.env.SQUARE_LOCATION_ID,
+          },
+          this.state.verificationDetails
+        )
+      ),
+    })
+      .then(async (res) => {
+        console.log(res)
+        if (!res.ok) {
+          const errorInfo = await res.json()
+          return await Promise.reject(errorInfo)
+        }
+        return res.json()
+      })
+      .then((data) => {
+        console.log("THEN with data", data)
+      })
+      .catch((err) => {
+        console.log("PAYMENT ERROR")
+        console.error(err)
+      })
+  }
+
+  handleDonationAmountClick(e, amount) {
+    console.log("handleDonationAmountClick")
+    e.preventDefault()
+
+    document
+      .querySelectorAll(".donation-amount-group .button")
+      .forEach((el) => {
+        el.classList.remove("is-selected")
+        el.classList.remove("is-success")
+      })
+    e.target.classList.add("is-selected")
+    e.target.classList.add("is-success")
+
+    const otherAmountField = document.getElementById("other-input-field")
+
+    if (amount === "other") {
+      otherAmountField.classList.remove("is-hidden")
+      amount = 0
+    } else {
+      otherAmountField.classList.add("is-hidden")
+    }
+    this.setState({ verificationDetails: { amount: amount } })
+  }
+
+  handleDonationAmountOtherKeyUp(e) {
+    console.log("handleDonationAmountOtherKeyUp")
+    e.target.value = e.target.value.replaceAll("/[^0-9.]/ig", "")
+    this.setState({ verificationDetails: { amount: e.target.value } })
+  }
+
+  handleSubmit(values, actions) {
+    console.log("handleSubmit:", values, actions)
+
+    document.getElementById("result-modal").classList.remove("is-hidden")
   }
 
   render() {
     return (
       <div>
-        <SquarePaymentForm
-          sandbox={this.props.sandbox || false}
-          applicationId={this.props.applicationId}
-          locationId={this.props.locationId}
-          cardNonceResponseReceived={this.cardNonceResponseReceived.bind(this)}
-          // TODO: more secure: createVerificationDetails={createVerificationDetails}
+        <Formik
+          initialValues={{
+            donationAmount: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+          }}
+          validationSchema={DonateFormSchema}
+          onSubmit={(values, actions) => this.handleSubmit(values, actions)}
         >
-          <fieldset className="sq-fieldset">
-            <CreditCardNumberInput />
-            <div className="sq-form-third">
-              <CreditCardCVVInput />
-            </div>
+          {({ errors, touched }) => (
+            <Form>
+              <div className="field buttons has-addons donation-amount-group">
+                <label className="label">Donation Amount</label>
+                <button
+                  className="button is-outlined"
+                  onClick={(e) => this.handleDonationAmountClick(e, 10)}
+                >
+                  $10
+                </button>
+                <button
+                  className="button is-outlined"
+                  onClick={(e) => this.handleDonationAmountClick(e, 25)}
+                >
+                  $25
+                </button>
+                <button
+                  className="button is-outlined selected"
+                  onClick={(e) => this.handleDonationAmountClick(e, 50)}
+                >
+                  $50
+                </button>
+                <button
+                  className="button is-outlined"
+                  onClick={(e) => this.handleDonationAmountClick(e, 100)}
+                >
+                  $100
+                </button>
+                <button
+                  className="button is-outlined other-button"
+                  onClick={(e) => this.handleDonationAmountClick(e, "other")}
+                >
+                  Other
+                </button>
+              </div>
+              <div id="other-input-field" className="field is-hidden">
+                <label className="label">Other Amount:</label>
+                <p className="control">
+                  <input
+                    id="other-input-amount"
+                    className="input"
+                    type="text"
+                    placeholder=""
+                    onKeyUp={(e) => this.handleDonationAmountOtherKeyUp(e)}
+                  />
+                </p>
+              </div>
 
-            <div className="sq-form-third">
-              <CreditCardExpirationDateInput />
-            </div>
+              <div className="field">
+                <label className="label">Cardholder Name</label>
+                <div className="control">
+                  <Field
+                    id="firstName"
+                    name="firstName"
+                    className="input"
+                    placeholder="First"
+                  />
+                </div>
+                {touched.firstName && errors.firstName && (
+                  <p className="help is-danger">{errors.firstName}</p>
+                )}
+                <div className="control">
+                  <Field
+                    id="lastName"
+                    name="lastName"
+                    className="input"
+                    placeholder="Last Name"
+                  />
+                </div>
+                {touched.lastName && errors.lastName && (
+                  <p className="help is-danger">{errors.lastName}</p>
+                )}
+              </div>
 
-            <div className="sq-form-third">
-              <CreditCardPostalCodeInput />
-            </div>
-          </fieldset>
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <Field
+                    id="email"
+                    name="email"
+                    className="input"
+                    type="email"
+                    placeholder="Email"
+                  />
+                  {touched.email && errors.email && (
+                    <p className="help is-danger">{errors.email}</p>
+                  )}
+                </div>
+              </div>
 
-          <p>I agree with the terms and conditions.*</p>
-
-          <CreditCardSubmitButton>Send</CreditCardSubmitButton>
-        </SquarePaymentForm>
-        <div className="sq-error-message">
-          {this.state.errorMessages.map((errorMessage) => (
-            <li key={`sq-error-${errorMessage}`}>{errorMessage}</li>
-          ))}
+              <div id="form-container">
+                <div className="field">
+                  <div id="sq-card-number"></div>
+                </div>
+                <div className="field">
+                  <div id="sq-expiration-date" className="third"></div>
+                </div>
+                <div className="field">
+                  <div id="sq-cvv" className="third"></div>
+                </div>
+                <div className="field">
+                  <div id="sq-postal-code" className="third"></div>
+                </div>
+                <div className="field">
+                  <button
+                    id="sq-creditcard"
+                    type="submit"
+                    className="button button-credit-card"
+                    onClick={this.handleSubmit}
+                  >
+                    {"Donate"}
+                    {this.state.verificationDetails.amount > 0 &&
+                      " $" + this.state.verificationDetails.amount}
+                  </button>
+                </div>
+              </div>
+              <div className="sq-error-message">
+                {this.state.errorMessages.map((errorMessage) => (
+                  <li key={`sq-error-${errorMessage}`}>{errorMessage}</li>
+                ))}
+              </div>
+            </Form>
+          )}
+        </Formik>
+        <div id="result-modal" className="modal is-hidden">
+          <div className="modal-background"></div>
+          <div className="modal-content is-error is-hidden">
+            We could not process your credit card information. Please check your
+            information before trying to submit again.’
+          </div>
+          <div className="modal-content is-success is-hidden">
+            Thank you for supporting {this.props.businessData.name}. You will be
+            emailed a receipt of your donation.
+          </div>
+          <div className="modal-content is-loading">Processing...</div>
+          <button className="modal-close is-large" aria-label="close">
+            Close
+          </button>
         </div>
       </div>
     )
@@ -94,9 +356,9 @@ export default class DonateForm extends React.Component {
 }
 
 DonateForm.propTypes = {
-  sandbox: PropTypes.bool,
+  // sandbox: PropTypes.bool,
   applicationId: PropTypes.string.isRequired,
   locationId: PropTypes.string.isRequired,
-  cardNonceResponseReceived: PropTypes.func.isRequired,
+  // cardNonceResponseReceived: PropTypes.func.isRequired,
   // createVerificationDetails: PropTypes.func.isRequired,
 }
--- src/templates/donate.js
+++ src/templates/donate.js
@@ -9,12 +9,6 @@ import SEO from "../components/SEO/SEO"
 import Content, { HTMLContent } from "../components/Content"
 import { FormattedMessage } from "react-intl"
 import DonateForm from "../components/DonateForm"
-import {
-  FaExclamationTriangle,
-  FaRegEnvelope,
-  FaCheck,
-  FaUser,
-} from "react-icons/fa"
 
 const DonatePageTemplate = ({
   title,
@@ -28,113 +22,6 @@ const DonatePageTemplate = ({
 }) => {
   const PageContent = contentComponent || Content
   const sel = select(langKey)
-
-  let verificationDetails = {
-    amount: 0,
-    currencyCode: "USD",
-    intent: "CHARGE",
-    billingContact: {
-      familyName: "BLANK",
-      givenName: "BLANK",
-      email: "BLANK",
-      country: "BLANK",
-      city: "BLANK",
-      addressLines: ["BLANK"],
-      postalCode: "BLANK",
-      phone: "BLANK",
-    },
-  }
-
-  const cardNonceResponseReceived = (
-    nonce,
-    cardData,
-    buyerVerificationToken // TODO be more secure
-  ) => {
-    console.log(
-      "nonce created: " +
-        nonce +
-        ", buyerVerificationToken: " +
-        buyerVerificationToken
-    )
-    console.log("cardData")
-    console.log(cardData)
-
-    document.getElementById("result-modal").classList.remove("is-hidden")
-
-    fetch("/.netlify/functions/square", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(
-        Object.assign(
-          {
-            nonce: nonce,
-            idempotency_key: uuidv4(),
-            location_id: process.env.SQUARE_LOCATION_ID,
-          },
-          verificationDetails
-        )
-      ),
-    })
-      .catch((error) => {
-        console.error(error)
-        alert("Network error: " + error)
-      })
-      .then((res) => {
-        console.log(res)
-        if (!res.ok) {
-          return res.json().then((errorInfo) => Promise.reject(errorInfo))
-        }
-        return res.json()
-      })
-      .catch((err) => {
-        console.log("PAYMENT ERROR")
-        console.error(err)
-      })
-  }
-
-  const handleDonationAmountButtonClick = (e, amount) => {
-    console.log("handleDonationAmountButtonClick", amount)
-    e.preventDefault()
-
-    const otherAmountField = document.getElementById("other-input-field")
-    if (e.currentTarget.id === "other-button") {
-      otherAmountField.classList.remove("is-hidden")
-    } else {
-      otherAmountField.classList.add("is-hidden")
-    }
-    verificationDetails.amount = amount
-  }
-
-  const handleDonationAmountButtonBlur = (e) => {
-    e.preventDefault()
-  }
-
-  const handleFormKeyUp = (e) => {
-    if (e.target.id === "first-name" || e.target.id == "last-name") {
-      e.target.value = e.target.value.trim()
-      if (e.target.value.length < 3) {
-        // UI error
-        return false
-      }
-
-      if (e.target.id === "first-name") {
-        verificationDetails.billingContact.givenName = e.target.value
-      } else {
-        verificationDetails.billingContact.familyName = e.target.value
-      }
-    } else if (e.target.id === "email") {
-      e.target.value = e.target.value.trim()
-      if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.value)) {
-        // UI error
-        return false
-      }
-
-      verificationDetails.billingContact.email = e.target.value
-    }
-  }
 
   return (
     <div className="content">
@@ -153,168 +40,18 @@ const DonatePageTemplate = ({
           </p>
         </div>
 
-        <div className="column is-full donate-step-1">
-          <div>
-            <h3>Step 1: Select Donation Amount</h3>
-          </div>
-
-          <div className="field is-grouped">
-            <button
-              className="button is-primary is-outlined"
-              onClick={(e) => handleDonationAmountButtonClick(e, 10)}
-              onBlur={(e) => handleDonationAmountButtonBlur(e)}
-            >
-              $10
-            </button>
-            <button
-              className="button is-primary is-outlined"
-              onClick={(e) => handleDonationAmountButtonClick(e, 25)}
-              onBlur={(e) => handleDonationAmountButtonBlur(e)}
-            >
-              $25
-            </button>
-            <button
-              className="button is-primary is-outlined"
-              onClick={(e) => handleDonationAmountButtonClick(e, 50)}
-              onBlur={(e) => handleDonationAmountButtonBlur(e)}
-            >
-              $50
-            </button>
-            <button
-              className="button is-primary is-outlined"
-              onClick={(e) => handleDonationAmountButtonClick(e, 100)}
-              onBlur={(e) => handleDonationAmountButtonBlur(e)}
-            >
-              $100
-            </button>
-            <button // Todo Other
-              id="other-button"
-              className="button is-primary is-outlined other-button is-hidden"
-            >
-              Other
-            </button>
-          </div>
-          {/* TODO: add "Other" amount field */}
-          <div id="other-input-field" className="field is-hidden">
-            <label className="label">Other:</label>
-            <p className="control">
-              <input
-                id="other-input-amount"
-                className="input"
-                type="text"
-                placeholder=""
-              />
-            </p>
-          </div>
-        </div>
-
-        <div className="column is-full donate-step-2">
-          <h3>Step 2: Enter Payment Information</h3>
-
-          <div className="columns is-full">
-            <div className="column is-half">
-              <div className="field">
-                <label className="label">Cardholder Name</label>
-                <div className="control has-icons-right">
-                  <input
-                    id="first-name"
-                    name="first-name"
-                    className="input"
-                    type="text"
-                    placeholder="First"
-                    onKeyUp={(e) => handleFormKeyUp(e)}
-                  />
-                  <span className="icon is-small is-right is-hidden">
-                    <FaExclamationTriangle />
-                  </span>
-                  <span className="icon is-small is-right is-hidden">
-                    <FaCheck />
-                  </span>
-                </div>
-                <div className="control has-icons-right">
-                  <input
-                    id="last-name"
-                    name="last-name"
-                    className="input"
-                    type="text"
-                    placeholder="Last"
-                    onKeyUp={(e) => handleFormKeyUp(e)}
-                  />
-                  <span className="icon is-small is-right is-hidden">
-                    <FaExclamationTriangle />
-                  </span>
-                  <span className="icon is-small is-right is-hidden">
-                    <FaCheck />
-                  </span>
-                </div>
-                <p className="help is-danger is-hidden">
-                  Please enter a valid name
-                </p>
-              </div>
-
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control has-icons-left has-icons-right">
-                  <input
-                    id="email"
-                    name="email"
-                    className="input"
-                    type="email"
-                    placeholder="Email"
-                    onKeyUp={(e) => handleFormKeyUp(e)}
-                  />
-                  <p className="help is-danger is-hidden">
-                    Please enter a valid email address
-                  </p>
-                  <span className="icon is-small is-left">
-                    <FaRegEnvelope />
-                  </span>
-                  <span className="icon is-small is-right is-hidden">
-                    <FaExclamationTriangle />
-                  </span>
-                  <span className="icon is-small is-right is-hidden">
-                    <FaCheck />
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="column donate-cc">
-              <DonateForm
-                sandbox={true}
-                applicationId={process.env.SQUARE_APPLICATION_ID}
-                locationId={process.env.SQUARE_LOCATION_ID}
-                cardNonceResponseReceived={cardNonceResponseReceived}
-                // createVerificationDetails={createVerificationDetails}
-              ></DonateForm>
-              <p className="footnote">
-                * By proceeding with your transaction, you understand that you
-                are making a donation to {businessData.name}. No goods or
-                services were exchanged for this donation.
-              </p>
-            </div>
-          </div>
+        <div className="column is-full">
+          <DonateForm
+            businessData={businessData}
+            applicationId={process.env.SQUARE_APPLICATION_ID}
+            locationId={process.env.SQUARE_LOCATION_ID}
+          />
         </div>
 
         <section className="section">
           <PageContent className="content" content={content} />
           <TagList tags={tags} langKey={langKey} />
         </section>
-
-        <div id="result-modal" className="modal is-hidden">
-          <div className="modal-background"></div>
-          <div className="modal-content is-error is-hidden">
-            We could not process your credit card information. Please check your
-            information before trying to submit again.’
-          </div>
-          <div className="modal-content is-success is-hidden">
-            Thank you for supporting {businessData.name}. You will be emailed a
-            receipt of your donation.
-          </div>
-          <div className="modal-content is-loading">Processing...</div>
-          <button className="modal-close is-large" aria-label="close">
-            Close
-          </button>
-        </div>
       </div>
     </div>
   )
@@ -322,7 +59,6 @@ const DonatePageTemplate = ({
 
 class DonatePage extends React.Component {
   render() {
-    console.log(this.props)
     let dataMarkdown = []
     let data
     if (this.props.data !== null) {
